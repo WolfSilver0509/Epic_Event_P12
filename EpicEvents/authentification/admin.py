50f0924a1a5b4a25e11ca1c6cdc1246ae836b613
Mydmoov <denizot.myriam@gmail.com>
2023-06-07 15:29:47 +0200

    Administration Django via admin abstract user

--- EpicEvents/authentification/admin.py
+++ EpicEvents/authentification/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from .models import User
+
+class UserAdmin(BaseUserAdmin):
+    # Définir l'ordre des champs dans la liste des utilisateurs
+    ordering = ['email']
+
+    # Définir les champs d'affichage dans la liste des utilisateurs
+    list_display = ['email', 'first_name', 'last_name', 'role', 'is_staff']
+
+    # Définir les champs de recherche pour la recherche d'utilisateurs
+    search_fields = ['email', 'first_name', 'last_name']
+
+    # Définir les filtres pour filtrer les utilisateurs par rôle et statut du personnel
+    list_filter = ['role', 'is_staff']
+
+    # Définir les détails de l'utilisateur à afficher
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Informations personnelles', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Informations supplémentaires', {'fields': ('role',)}),
+    )
+
+    # Définir les champs à afficher lors de la création d'un nouvel utilisateur
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+    # Définir le modèle utilisateur personnalisé
+    model = User
+
+# Enregistrer le modèle utilisateur personnalisé dans l'interface d'administration
+admin.site.register(User, UserAdmin)
+# Supprimer le modèle utilisateur par défaut de l'interface d'administration
+admin.site.unregister(Group)
+
--- EpicEvents/epic/admin.py
+++ EpicEvents/epic/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Client, Contract, Event
 
-# Register your models here.
+# Chaque modèle (Client, Contract, Event) est enregistré dans
+# l'interface d'administration en utilisant le décorateur admin.register.
+# Le décorateur admin.register prend en paramètre le modèle à enregistrer.
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email', 'phone', 'mobile', 'company_name']
+    search_fields = ['first_name', 'last_name', 'email', 'company_name']
+
+@admin.register(Contract)
+class ContractAdmin(admin.ModelAdmin):
+    list_display = ['contact_ventes', 'client', 'status', 'amount', 'payment_due']
+    search_fields = ['contact_ventes__email', 'client__first_name', 'client__last_name']
+    list_filter = ['status']
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['contract', 'event_date', 'event_status', 'attendees']
+    search_fields = ['contract__client__first_name', 'contract__client__last_name']
+    list_filter = ['event_status']
